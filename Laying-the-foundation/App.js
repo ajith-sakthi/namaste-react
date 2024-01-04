2c3230874c904b909c1b1e99df69d3d1be395e5e
ajith-sakthi <ajithkumarmyai@gmail.com>
2024-01-04 11:50:37 +0530

    episode-03

--- Laying-the-foundation/App.js
+++ Laying-the-foundation/App.js
@@ -2,31 +2,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent= React.createElement(
-    "div",
-    {id:"parent"},
-    [ React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag")]
-        ), 
-        React.createElement(
-            "div", 
-            {id:"child2"},[
-           React.createElement("h1",{},"I am child2 h1 tag") ,
-           React.createElement("h2",{},"I am child2 h2 tag")
-        ]) ]
-        );
+
+const heading=React.createElement("h1",{id:"heading"},"vanakkam by React")
+
+const jsxheading=(<h1 id="heading">Vanakkam by jsx </h1>)
+
+const Title =() => (
+    <h1 id="heading">
+    Vanakkam React Title component
+    </h1>)
+
+
+//Functionnal component
+const HeadingComponent = () =>( 
+    <div id="container">
+        <Title />
+        <h1> 
+        Functional Heading Component
+        </h1> 
+    </div>
+    ) 
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<HeadingComponent/>);
 
 
 
-
-// const heading=React.createElement(
-//     "h1",
-//     {id:"heading", xyz:"abc"}, // create an attribute for element
-//     "Hello world from React"); // heading returns object, heading- React element
-console.log(parent);
-const root=ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(parent);
